74af28f281bc1c811a9c833f52edfd0a35ccef94
hafizjavaid <hafizjaidi786@gmail.com>
2024-09-24 22:33:37 +0500

    nuxt icon and iconify setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt', '@nuxt/icon'],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
