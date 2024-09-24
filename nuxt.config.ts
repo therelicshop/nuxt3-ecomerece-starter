3da60f8ee14bc0ec6e17b3d010ee8e151a1b2cc7
hafizjavaid <hafizjaidi786@gmail.com>
2024-09-24 22:51:24 +0500

    Cloudinary Setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,27 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt', '@nuxt/icon'],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt', '@nuxt/icon', "@nuxtjs/cloudinary"],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
-  }
+  },
+  runtimeConfig: {
+    githubId: '',
+    githubSecret: '',
+    stripeSecret: '',
+    stripeWebhookSecret: '',
+    public: {
+      cloudinaryCloudName: '',
+      uploadPreset: '',
+      stripeKey: '',
+    },
+    cloudinaryApiKey: '',
+    cloudinaryApiSecret: '',
+  },
+  cloudinary: {
+    cloudName: process.env.NUXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    uploadPreset: process.env.NUXT_PUBLIC_UPLOAD_PRESET,
+    apiKey: process.env.NUXT_CLOUDINARY_API_KEY,
+  },
 })
