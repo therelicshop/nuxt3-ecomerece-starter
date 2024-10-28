ecc5bd5b73881cb248b0e3df3c7e6024adb81202
Hafiz Javaid Iqbal <hafizjaidi786@gmail.com>
2024-10-28 22:35:23 +0500

    Removing Lucia and installing Auth utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt', '@nuxt/icon', "@nuxtjs/cloudinary"],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@vueuse/nuxt',
+    '@nuxt/icon',
+    "@nuxtjs/cloudinary",
+    'nuxt-auth-utils',
+  ],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
